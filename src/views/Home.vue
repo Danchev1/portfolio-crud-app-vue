9cfbf4326ab7d1f192033add2831b9fb25739530
yordandanchev <yordangdanchev@gmail.com>
2019-06-22 22:55:30 +0300

    - Vue project init - Axios and Axios mock added - Bootstrap-vue - About page and basic layout component added

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-container tag="main" class="home-page">
+    <b-row>
+      <b-col>
+        Home page
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
