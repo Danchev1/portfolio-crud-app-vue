256bc5bdc3a856ef8f89ece8fd3c7bb1229f0b12
yordandanchev <yordangdanchev@gmail.com>
2019-06-23 19:25:13 +0300

    - functionality added: delete project, create project, search for a project, http req the packages.json

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 import Vue from 'vue';
-import BootstrapVue from 'bootstrap-vue';
+/* import BootstrapVue from 'bootstrap-vue'; */
+import {
+  LayoutPlugin,
+  CardPlugin,
+  JumbotronPlugin,
+  NavPlugin,
+  ButtonPlugin,
+  FormInputPlugin,
+} from 'bootstrap-vue';
+
 import App from '@/App.vue';
 import router from '@/router/router';
-import store from '@/store/store';
 import '@/assets/scss/main.scss';
 
-Vue.use(BootstrapVue);
+Vue.use(LayoutPlugin);
+Vue.use(CardPlugin);
+Vue.use(JumbotronPlugin);
+Vue.use(NavPlugin);
+Vue.use(ButtonPlugin);
+Vue.use(FormInputPlugin);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: h => h(App),
 }).$mount('#app');
